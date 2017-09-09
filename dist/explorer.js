7a0e9edb849acb272fcf5b1b99514f1186e93cb0
Matt Hamlin <hamlim@outlook.com>
2017-09-09 17:20:27 -0400

    Revert "Use react without a build step"
    This reverts commit 362848b61b1574f731112c10a0d1ed79c3e83b67.

--- dist/explorer.js
+++ dist/explorer.js
@@ -1,33 +1,31 @@
-const init = () => {
-  'use strict'
+document.addEventListener('DOMContentLoaded', function() {
+  'use strict';
 
-  const editor = window.ace.edit('editor')
-  editor.setTheme('ace/theme/monokai')
-  editor.getSession().setMode('ace/mode/javascript')
+  const editor = window.ace.edit('editor');
+  editor.setTheme('ace/theme/monokai');
+  editor.getSession().setMode('ace/mode/javascript');
 
-  const binary = window.ace.edit('binary')
-  binary.setReadOnly(true)
+  const binary = window.ace.edit('binary');
+  binary.setReadOnly(true);
 
-  const compileButton = document.getElementById('compile')
+  console.log('test');
+
+  const compileButton = document.getElementById('compile');
   compileButton.addEventListener('click', () => {
-    const src = editor.getValue()
-    const wasm = window.Walt.getAst(src)
-    binary.$blockScrolling = Infinity
-    binary.setValue(wasm.debug(), -1)
-    WebAssembly.instantiate(wasm.buffer()).then(result => {
-      console.log('Module', result.module)
-      console.log('Instance', result.instance)
-      const exports = result.instance.exports
-      console.log(
-        'Exports',
-        Object.keys(result.instance.exports).forEach(
-          k =>
-            typeof exports[k] === 'function'
-              ? console.log(`${k} => ${exports[k]()}`)
-              : console.log(`${k} => ${exports[k]}`),
-        ),
-      )
-    })
-  })
-}
-export default init
+    const src = editor.getValue();
+    const wasm = window.Walt.getAst(src);
+    binary.$blockScrolling = Infinity;
+    binary.setValue(wasm.debug(), -1);
+    WebAssembly.instantiate(wasm.buffer())
+      .then(result => {
+        console.log('Module', result.module);
+        console.log('Instance', result.instance);
+        const exports = result.instance.exports;
+        console.log('Exports', Object.keys(result.instance.exports).forEach(
+          k => typeof exports[k] === 'function' ?
+          console.log(`${k} => ${exports[k]()}`) :
+          console.log(`${k} => ${exports[k]}`)
+        ));
+      });
+  });
+});
