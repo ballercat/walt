e6b893c987ec34badcc1bdf6118910a1b9c68706
dandrei <dandrei@axway.com>
2018-01-05 11:26:25 +0200

    Move Comments module scoped variables into a single closure (#2)

--- packages/walt-compiler/src/tokenizer/comments/index.js
+++ packages/walt-compiler/src/tokenizer/comments/index.js
@@ -11,44 +11,49 @@ import {
   everything,
 } from "./constants";
 
-let isMultiline: boolean = false;
-let previous: string;
+const parser = () => {
+  let isMultiline: boolean = false;
+  let previous: string;
 
-const isComment = (current: string) => {
-  switch (`${previous}${current}`) {
-    case MULTI_LINE_END: {
-      isMultiline = false;
-      return isComment;
-    }
-    case MULTI_LINE_START: {
-      isMultiline = true;
-      return isComment;
-    }
-    case SINGLE_LINE: {
-      return everything;
-    }
-    default: {
-      if (isMultiline) {
-        previous = current;
+  const isComment = (char: string) => {
+    switch (`${previous}${char}`) {
+      case MULTI_LINE_END: {
+        isMultiline = false;
         return isComment;
       }
+      case MULTI_LINE_START: {
+        isMultiline = true;
+        return isComment;
+      }
+      case SINGLE_LINE: {
+        return everything;
+      }
+      default: {
+        if (isMultiline) {
+          previous = char;
+          return isComment;
+        }
+      }
     }
-  }
+  };
+
+  const maybeComment = (current: string) => {
+    const buffer: string = previous;
+    previous = current;
+
+    if (
+      current === SLASH ||
+      isMultiline ||
+      COMMENT_IDENTIFIERS.indexOf(`${buffer}${current}`) > -1
+    ) {
+      return isComment;
+    }
+
+    return null;
+  };
+
+  return token(maybeComment, Syntax.Comment);
 };
 
-const maybeComment = (current: string) => {
-  const buffer: string = previous;
-  previous = current;
-  if (
-    current === SLASH ||
-    COMMENT_IDENTIFIERS.indexOf(`${buffer}${current}`) > -1 ||
-    isMultiline
-  ) {
-    return isComment;
-  }
-
-  return null;
-};
-
-const commentParser = token(maybeComment, Syntax.Comment);
+const commentParser = parser();
 export default commentParser;
