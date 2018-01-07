7ba28cd888eda8061a9ac4867e5ba513f04b9ff9
Arty Buldauskas <abuldauskas@wayfair.com>
2018-01-07 10:51:46 -0500

    speed up CI builds slightly

--- packages/walt-compiler/src/__tests__/type-conversion-spec.js
+++ packages/walt-compiler/src/__tests__/type-conversion-spec.js
@@ -5,22 +5,22 @@ import compose from "../utils/compose";
 const walt = `export function constant(): f32 {
   return 0.5;
 }
-export function variableTypecast(): f32 {
-  const x: i32 = 1;
+export function variableTypecast(x: i32): f32 {
   return (x: f32) + 5.0;
 }
-export function _32IntTypecast(): i32 {
-  return (2.0: i32) + 2;
+export function _32IntTypecast(x: f32): i32 {
+  return (x: i32) + 2;
 }
-export function _32FloatTypecast(): f32 {
-  return 2.5 + (2: f32) + 0.5;
+export function _32FloatTypecast(x: i32): f32 {
+  return 2.5 + (x: f32) + 0.5;
 }
+// We cannot call this function from the outside with a i64 number :/
 export function _64IntTypecast(): i32 {
   const x: i64 = 2;
-  return 2 + (x: i32);
+  return (2 * x): i32;
 }
 export function _64FloatTypecast(x: f64): f32 {
-  return 2 * (x : f32);
+  return (2 * x): f32;
 }
 export function promotions(): f32 {
   return 2.5 + 2 + 0.5 * (10/ 5);
@@ -32,9 +32,9 @@ test("typecasts work", t => {
   t.snapshot(wasm);
   return WebAssembly.instantiate(compile(walt)).then(result => {
     t.is(result.instance.exports.constant(), 0.5);
-    t.is(result.instance.exports.variableTypecast(), 6);
-    t.is(result.instance.exports._32IntTypecast(), 4);
-    t.is(result.instance.exports._32FloatTypecast(), 5);
+    t.is(result.instance.exports.variableTypecast(2), 7);
+    t.is(result.instance.exports._32IntTypecast(2.0), 4);
+    t.is(result.instance.exports._32FloatTypecast(2), 5);
     t.is(result.instance.exports._64IntTypecast(), 4);
     t.is(result.instance.exports._64FloatTypecast(2), 4);
     t.is(result.instance.exports.promotions(), 5.5);
