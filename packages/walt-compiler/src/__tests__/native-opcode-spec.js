32301daeeea95e5f7ff84b1d250f1f69e2b38a80
Arty Buldauskas <abuldauskas@wayfair.com>
2018-04-29 19:56:25 -0400

    fix a native spec

--- packages/walt-compiler/src/__tests__/native-opcode-spec.js
+++ packages/walt-compiler/src/__tests__/native-opcode-spec.js
@@ -1,11 +1,5 @@
 import test from "ava";
 import { harness } from "../utils/test-utils";
-import { readFileSync } from "fs";
 import { join } from "path";
 
-const source = readFileSync(
-  join(__dirname, "./native-opcode-spec.walt"),
-  "utf8"
-);
-
-test("native opcodes", harness(source));
+test("native opcodes", harness(join(__dirname, "./native-opcode-spec.walt")));
