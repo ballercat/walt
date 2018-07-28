9b1799fa0a6a4a07bf9d988b5e4446ecaed50629
Arty Buldauskas <abuldauskas@wayfair.com>
2018-07-28 09:31:31 -0400

    fix memory & table exports

--- packages/walt-compiler/src/generator/export.js
+++ packages/walt-compiler/src/generator/export.js
@@ -1,16 +1,30 @@
 // @flow
 import { GLOBAL_INDEX, FUNCTION_INDEX } from "../semantics/metadata";
-import { EXTERN_GLOBAL, EXTERN_FUNCTION } from "../emitter/external_kind";
+import {
+  EXTERN_GLOBAL,
+  EXTERN_MEMORY,
+  EXTERN_TABLE,
+  EXTERN_FUNCTION,
+} from "../emitter/external_kind";
 import type { NodeType, IntermediateExportType } from "./flow/types";
+
+const externaKindMap = {
+  Memory: EXTERN_MEMORY,
+  Table: EXTERN_TABLE,
+};
 
 export default function generateExport(node: NodeType): IntermediateExportType {
   const functionIndexMeta = node.meta[FUNCTION_INDEX];
   const globalIndexMeta = node.meta[GLOBAL_INDEX];
 
   if (globalIndexMeta != null) {
+    const kind = externaKindMap[String(node.type)] || EXTERN_GLOBAL;
+    const index = [EXTERN_MEMORY, EXTERN_TABLE].includes(kind)
+      ? 0
+      : globalIndexMeta;
     return {
-      index: globalIndexMeta,
-      kind: EXTERN_GLOBAL,
+      index,
+      kind,
       field: node.value,
     };
   }
