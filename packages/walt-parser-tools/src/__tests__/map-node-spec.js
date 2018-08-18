ab4a1158c758ec59102bd92a22f06b79dafa2d12
Arty Buldauskas <abuldauskas@wayfair.com>
2018-08-18 16:22:29 +0000

    setup a very basic script

---
+++ packages/walt-parser-tools/src/__tests__/map-node-spec.js
@@ -0,0 +1,38 @@
+import test from 'ava';
+import Syntax from '../../Syntax';
+import mapNode from '../map-node';
+
+const mockNodeString =
+  '{"Type":"BinaryExpression","value":"+","range":[null,{"line":1,"col":8}],"meta":[],"params":[{"Type":"ArraySubscript","value":"[","range":[null,{"line":1,"col":4}],"meta":[],"params":[{"Type":"Identifier","value":"b","range":[{"line":1,"col":0},{"line":1,"col":1}],"meta":[{"payload":0,"type":"global/index"}],"params":[],"type":"i32"},{"Type":"Constant","value":"1","range":[{"line":1,"col":2},{"line":1,"col":3}],"meta":[],"params":[],"type":"i32"}],"type":"i32"},{"Type":"Constant","value":"5","range":[{"line":1,"col":7},{"line":1,"col":8}],"meta":[],"params":[],"type":"i32"}],"type":"i32"}';
+
+test('mapping a node results in a new node', t => {
+  const node = JSON.parse(mockNodeString);
+  const newNode = mapNode({
+    [Syntax.BinaryExpression]: binaryNode => {
+      if (binaryNode.value === '+') {
+        return {
+          ...binaryNode,
+          value: '-',
+        };
+      }
+      return binaryNode;
+    },
+  })(node);
+
+  t.not(node, newNode);
+});
+
+test('wildcards', t => {
+  const node = JSON.parse(mockNodeString);
+
+  const newNode = mapNode({
+    '*': n => ({ ...n }),
+  })(node);
+
+  t.not(node, newNode);
+});
+
+test('null nodes', t => {
+  const newNode = mapNode({})(null);
+  t.is(null, newNode);
+});
---
+++ packages/walt-parser-tools/src/map-node.js
@@ -0,0 +1,69 @@
+const extend = require('xtend');
+
+const identity = id => id;
+
+function map(visitors) {
+  function mapper(input) {
+    if (!Array.isArray(input)) {
+      throw new Error(
+        'Transform must be used on an Array. Received ' + JSON.stringify(input)
+      );
+    }
+    const visitor = (() => {
+      const [node] = input;
+      if (node.Type in visitors && typeof visitors[node.Type] === 'function') {
+        return visitors[node.Type];
+      }
+      return identity;
+    })();
+
+    if (visitor.length === 2) {
+      return visitor(input, mapper);
+    }
+
+    const [node, ...rest] = visitor(input);
+    const params = node.params
+      .filter(Boolean)
+      .map(child => mapper([child, ...rest]));
+
+    return extend(node, { params });
+  }
+
+  return mapper;
+}
+
+// This should maybe be it's own module.
+export { map };
+
+export default function mapNode(visitor) {
+  const nodeMapper = node => {
+    if (node == null) {
+      return node;
+    }
+
+    const mappingFunction = (() => {
+      if ('*' in visitor && typeof visitor['*'] === 'function') {
+        return visitor['*'];
+      }
+
+      if (node.Type in visitor && typeof visitor[node.Type] === 'function') {
+        return visitor[node.Type];
+      }
+      return identity;
+    })();
+
+    if (mappingFunction.length === 2) {
+      return mappingFunction(node, nodeMapper);
+    }
+
+    const mappedNode = mappingFunction(node);
+    const params = mappedNode.params.map(nodeMapper);
+
+    return {
+      ...mappedNode,
+      params,
+    };
+  };
+
+  return nodeMapper;
+}
