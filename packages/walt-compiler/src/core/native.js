3f3dda4dffd27b56a0982b870d28593965eb1466
Arty Buldauskas <abuldauskas@wayfair.com>
2018-09-17 13:08:10 +0000

    Improve coverage

--- packages/walt-compiler/src/core/native.js
+++ packages/walt-compiler/src/core/native.js
@@ -5,24 +5,6 @@ export default function nativePlugin() {
   return {
     semantics() {
       return {
-        ArraySubscripts: next => (args, transform) => {
-          const [node, context] = args;
-          const [identifier, field] = node.params;
-          if (
-            identifier.Type === Syntax.Type &&
-            field.Type === Syntax.FunctionCall
-          ) {
-            return {
-              ...node,
-              Type: Syntax.NativeMethod,
-              type: identifier.value,
-              value: identifier.value + '.' + field.value,
-              params: field.params.map(p => transform([p, context])),
-            };
-          }
-
-          return next(args);
-        },
         FunctionCall: next => (args, transform) => {
           const [node, context] = args;
           const [id, ...fnArgs] = node.params;
