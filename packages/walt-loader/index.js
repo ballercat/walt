18896f3bc495847b9f0b28a97ce2c59038e5a2ec
Arty Buldauskas <abuldauskas@wayfair.com>
2018-09-22 23:14:50 +0000

    patch loader

--- packages/walt-loader/index.js
+++ packages/walt-loader/index.js
@@ -1,8 +1,8 @@
 const wasmLoader = require("wasm-loader");
-const waltCompiler = require("walt-compiler");
+const { compile } = require("walt-compiler");
 
 module.exports = function waltLoader(source) {
-  const buffer = waltCompiler.default(source);
+  const buffer = compile(source).buffer();
   buffer.length = buffer.byteLength;
   // The new Buffer step can/should be skipped here, maybe
   wasmLoader.call(this, new Buffer(buffer));
