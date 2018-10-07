e48c4e694572b5313c8819adf96b4f58a8ab12ba
Arty Buldauskas <abuldauskas@wayfair.com>
2018-10-07 16:01:16 +0000

    Add coverage and comments

--- packages/walt-compiler/src/index.js
+++ packages/walt-compiler/src/index.js
@@ -87,15 +87,15 @@ export const compile = (source: string, config: ConfigType) => {
   // Generate plugin instances and sort them by the extended compiler phase
   const plugins = extensions.reduce(
     (acc, plugin) => {
-      const instance = plugin(options);
+      // Default plugins to a specific to ensure correctness
+      const instance = {
+        semantics: () => ({}),
+        grammar: () => ({ ParserRules: [] }),
+        ...plugin(options),
+      };
 
-      if (typeof instance.grammar === 'function') {
-        acc.grammar.push(instance.grammar);
-      }
-
-      if (typeof instance.semantics === 'function') {
-        acc.semantics.push(instance.semantics);
-      }
+      acc.grammar.push(instance.grammar);
+      acc.semantics.push(instance.semantics);
 
       return acc;
     },
