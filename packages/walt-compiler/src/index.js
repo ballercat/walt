6a0e3539bfb9459e5679be1fb876d120189e537c
Arty Buldauskas <abuldauskas@wayfair.com>
2018-10-12 13:20:41 +0000

    Bump source version number

--- packages/walt-compiler/src/index.js
+++ packages/walt-compiler/src/index.js
@@ -30,7 +30,7 @@ export {
   walkNode,
   mapNode,
 };
-export const VERSION = '0.16.0';
+export const VERSION = '0.16.3';
 
 // Used for debugging purposes
 export const getIR = (source: string, config: ConfigType) => {
