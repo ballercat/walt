fee60836a21dd6cbacb73826d523ddd97d8d7ba0
jtiscione <jason.tiscione@dish.com>
2018-11-12 14:37:12 -0700

    Lots more fixes in wave.js, added non-working stepJS()

--- packages/walt-explorer/src/examples/wave.js
+++ packages/walt-explorer/src/examples/wave.js
@@ -15,20 +15,28 @@ function compile(buffer) {
     getCanvasHeight,
     log: (val) => {
       console.log(val);
-    },
-    log16: (val) => {
-      if (val < 0) {
-        val = 0x100000000 + val;
-      }
-      console.log(val.toString(16));
     }
   };
+
+
+  const brushMatrix = [];
+  const brushMatrixRadius = 28;
+  for (let p = -brushMatrixRadius; p <= brushMatrixRadius; p++) {
+    const row = [];
+    brushMatrix.push(row);
+    for (let q = -brushMatrixRadius; q <= brushMatrixRadius; q++) {
+      const element = Math.floor(0x3FFFFFFF * Math.exp(-0.05 * ((p * p) + (q * q))));
+      row.push(element);
+    }
+  }
+
   return WebAssembly.instantiate(buffer, { env }).then(result => {
 
     const exports = result.instance.exports;
     const memory = exports.memory;
 
-    const wh = canvas.width * canvas.height;
+    const { width, height } = canvas;
+    const wh = width * height;
     const pages = 1 + ((20 * wh) >> 16);
     memory.grow(pages);
     const heap = memory.buffer;
@@ -36,43 +44,118 @@ function compile(buffer) {
 
     const imageArray = new Uint8ClampedArray(heap, HEAP_START, 4 * wh);
     const forceArray = new Int32Array(heap, HEAP_START + 4 * wh, wh);
-    // const statusArray = new Int32Array(heap, HEAP_START + 8 * wh, wh);
+    const statusArray = new Int32Array(heap, HEAP_START + 8 * wh, wh);
+    const uArray = new Int32Array(heap, HEAP_START + 12 * wh, wh);
+    const velArray = new Int32Array(heap, HEAP_START + 16 * wh, wh);
 
     let lastMouseX = null;
     let lastMouseY = null;
 
+    function applyBrush(x, y) {
+      const applyCap = (x) => (x < -0x40000000 ? -0x40000000 : (x > 0x3FFFFFFF ? 0x3FFFFFFF : x));
+      const { width, height } = canvas;
+      for (let p = -brushMatrixRadius; p <= brushMatrixRadius; p++) {
+        const targetY = y + p;
+        if (targetY <= 0 || targetY >= height - 1) {
+          continue;
+        }
+        for (let q = -brushMatrixRadius; q <= brushMatrixRadius; q++) {
+          const targetX = x + q;
+          if (targetX <= 0 || targetX >= width - 1) {
+            continue;
+          }
+          const brushValue = brushMatrix[p + brushMatrixRadius][q + brushMatrixRadius];
+          const targetIndex = targetY * width + targetX;
+          forceArray[targetIndex] += brushValue;
+          forceArray[targetIndex] = applyCap(forceArray[targetIndex]);
+        }
+      }
+    }
+
     canvas.onmousedown = (e) => {
       e.preventDefault();
       let bbox = canvas.getBoundingClientRect();
-      const mouseX = Math.round(e.clientX - bbox.left * (canvas.width / bbox.width));
-      const mouseY = Math.round(e.clientY - bbox.top * (canvas.height / bbox.height));
+      const mouseX = Math.round(e.clientX - bbox.left * (width / bbox.width));
+      const mouseY = Math.round(e.clientY - bbox.top * (height / bbox.height));
       lastMouseX = mouseX;
       lastMouseY = mouseY;
-      const targetIndex = lastMouseY * canvas.width + lastMouseX;
-      forceArray[targetIndex] = 0x3FFFFFFF;
+      applyBrush(mouseX, mouseY);
     };
 
-    canvas.omousemove = (e) => {
+    canvas.onmousemove = (e) => {
       e.preventDefault();
       let bbox = canvas.getBoundingClientRect();
-      const mouseX = Math.round(e.clientX - bbox.left * (canvas.width / bbox.width));
-      const mouseY = canvas.height - (Math.round(e.clientY - bbox.top * (canvas.height / bbox.height)));
+      const mouseX = Math.round(e.clientX - bbox.left * (width / bbox.width));
+      const mouseY = Math.round(e.clientY - bbox.top * (height / bbox.height));
       if (lastMouseX !== null && lastMouseY !== null) {
         const r = Math.sqrt((mouseX - lastMouseX) * (mouseX - lastMouseX) + (mouseY - lastMouseY) * (mouseY - lastMouseY));
-        for (let t = 0; t <= r; t++) {
+        for (let t = 0; t <= r; t += 5) {
           const currX = Math.round(lastMouseX + ((mouseX - lastMouseX) * (t / r)));
           const currY = Math.round(lastMouseY + ((mouseY - lastMouseY) * (t / r)));
-          forceArray[currY * canvas.width + currX] = 0x3FFFFFFF;
+          applyBrush(currX, currY);
+          forceArray[currY * width + currX] = 0x3FFFFFFF;
         }
-        lastMouseX = e.clientX;
-        lastMouseY = e.clientY;
+        applyBrush(mouseX);
+        applyBrush(mouseY);
+        lastMouseX = mouseX;
+        lastMouseY = mouseY;
       }
     };
 
-    canvas.onmouseover = canvas.onmouseout = canvas.onmouseup = (e) => {
+    canvas.onmouseout = canvas.onmouseup = (e) => {
       e.preventDefault();
-      lastMouseX = 0;
-      lastMouseY = 0;
+      lastMouseX = null;
+      lastMouseY = null;
+    };
+
+    const stepJS = () => {
+
+      console.log('width', width);
+      console.log('height', height);
+
+      const applyCap = (x) => (x < -0x40000000 ? -0x40000000 : (x > 0x3FFFFFFF ? 0x3FFFFFFF : x));
+
+      const toRGB = (x) => {
+        const val = x >> 22;
+        if (val < 0) {
+          return ((-(val + 1)) | 0xFF000000);
+        }
+        return (((val << 8) | (val << 16)) | 0xFF000000); // cyan
+      };
+
+      for (let i = 0; i < height; i += 1) {
+        for (let j = 0; j < width; j += 1) {
+          if (i === 0 || j === 0 || i === height - 1 || j === width - 1) {
+            statusArray[i * width + j] = 1;
+          }
+        }
+      }
+      for (let i = 0; i < wh; i += 1) {
+        if (statusArray[i] === 0) {
+          const uCen = uArray[i];
+          const uNorth = uArray[i - width];
+          const uSouth = uArray[i + width];
+          const uEast = uArray[i - 1];
+          const uWest = uArray[i + 1];
+          const uxx = (((uWest + uEast) >> 1) - uCen);
+          const uyy = (((uNorth + uSouth) >> 1) - uCen);
+          velArray[i] = applyCap(velArray[i] + (uxx >> 1) + (uyy >> 1));
+        }
+      }
+      for (let i = 0; i < wh; i += 1) {
+        if (statusArray[i] === 0) {
+          const f = forceArray[i];
+          uArray[i] = applyCap(f + applyCap(uArray[i] + velArray[i]));
+          forceArray[i] = f >> 1;
+        }
+      }
+      for (let i = 0; i < wh; i += 1) {
+        if (statusArray[i] === 1) {
+          imageArray[i] = 0xFFFF0000;
+        } else {
+          imageArray[i] = toRGB(uArray[i]);
+        }
+      }
     };
 
     let stopped = false;
@@ -80,12 +163,13 @@ function compile(buffer) {
     function step() {
       if (!stopped) {
         exports.step();
+        // stepJS();
         imgData.data.set(imageArray);
         ctx.putImageData(imgData, 0, 0);
-        window.requestAnimationFrame(step);
+        setTimeout(step, 0);
       }
     }
-    window.requestAnimationFrame(step);
+    step();
     return function() {
       stopped = true;
     }
