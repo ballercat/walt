e92227443b1e7bdee0d2358c9c288e054251961d
Arty Buldauskas <arthurbuldauskas@gmail.com>
2018-11-25 01:19:54 +0000

    Patch incorrect access logic
    Access operator '.' was not using the correct tye for the accessed
    field value. It was using the parent type instead of the fields.

--- packages/walt-compiler/src/generator/access.js
+++ packages/walt-compiler/src/generator/access.js
@@ -5,19 +5,21 @@ import type { GeneratorType } from './flow/types';
 import mapSyntax from './map-syntax';
 
 const generateAccess: GeneratorType = (node, parent) => {
+  const [, field] = node.params;
   const block = node.params.map(mapSyntax(parent)).reduce(mergeBlock, []);
   // The sequence of opcodes to perfrom a memory load is
   // get(Local|Global) base, i32Const offset[, i32Const size, i32Mul ], i32Add
   block.push({ kind: opcode.i32Add, params: [] });
 
   block.push({
-    kind: opcode[String(node.type) + 'Load'],
+    kind: opcode[String(field.type) + 'Load'],
     params: [
       // Alignment
       2,
       // Memory. Always 0 in the WASM MVP
       0,
     ],
+    debug: `access ${node.value} of type ${String(field.type)}`,
   });
 
   return block;
